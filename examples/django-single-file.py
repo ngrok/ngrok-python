ff0e033f57d946bc7ece6a08e2f44073ce690a14
bobzilladev <bob@ngrok.com>
2023-04-03 15:47:21 -0400

    add django to examples

---
+++ examples/django-single-file.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# Adapted from: https://arunrocks.com/django-application-in-one-file/
+
+from django.conf import settings
+from django.core.management import execute_from_command_line
+from django.http import HttpResponse
+from django.urls import path
+import asyncio, logging, ngrok, os, sys
+
+# Set env variable to protect against the autoreloader.
+if os.getenv("NGROK_TUNNEL_RUNNING") is None:
+    os.environ["NGROK_TUNNEL_RUNNING"] = "true"
+    logging.basicConfig(level=logging.INFO)
+
+    async def setup():
+        tunnel = await ngrok.default()
+        tunnel.forward_tcp("localhost:8080")
+
+    asyncio.run(setup())
+
+
+async def home(request):
+    response = HttpResponse("Hello")
+    return response
+
+
+settings.configure(
+    ALLOWED_HOSTS=["*"],  # Disable host header validation
+    DEBUG=True,
+    MIDDLEWARE=[
+        "django.middleware.common.CommonMiddleware"
+    ],  # CommonMiddleware adds Content-Length header
+    ROOT_URLCONF=__name__,
+    SECRET_KEY="a-bad-secret",  # Insecure! Change this
+)
+urlpatterns = [path("", home)]
+
+execute_from_command_line([__name__, "runserver", "8080"])
---
+++ examples/djangosite/manage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Added by ngrok"""
+    # This block handles 'make rundjangosite' which uses this manage.py as the entry point.
+    # Set env variable to protect against the autoreloader.
+    if os.getenv("NGROK_TUNNEL_RUNNING") is None:
+        os.environ["NGROK_TUNNEL_RUNNING"] = "true"
+        import asyncio, multiprocessing, ngrok
+
+        async def setup():
+            listen = sys.argv[2] if len(sys.argv) > 2 else "localhost:8000"
+            tunnel = await ngrok.default()
+            print(f"Forwarding to {listen} from ingress url: {tunnel.url()}")
+            tunnel.forward_tcp(listen)
+
+        asyncio.run(setup())
+    """End added by ngrok"""
+
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosite.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
