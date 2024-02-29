73652c59e73e0a3b4d1d9313a09f85966347f261
Josh Hertlein <jmh@ngrok.com>
2024-02-29 13:55:11 -0600

    add example of decorator use

--- ngrok_extra/ngrok_extra/test_utils.py
+++ ngrok_extra/ngrok_extra/test_utils.py
@@ -6,6 +6,8 @@ import ngrok_extra.utils as n
 import socket
 import subprocess
 from urllib.parse import urlparse
+
+
 class TestNgrokUtils(unittest.TestCase):
     def test_scoped_endpoint(self):
         with n.scoped_endpoint(9000, authtoken_from_env=True, proto="tcp") as endpoint:
@@ -20,7 +22,13 @@ class TestNgrokUtils(unittest.TestCase):
             stdout, stderr = nc.communicate()
             self.assertEqual("doot doot", stdout.decode("utf-8").strip())
 
+    def test_decorator_scoped_endpoint(self):
+        @n.scoped_endpoint(9000, authtoken_from_env=True)
+        def foo():
+            print("tunnel lives for the lifetime of the function")
+            print("mostly only useful if you don't need the endpoint object to get its URL. reserved domains etc?")
 
+        foo()
 
 
 if __name__ == '__main__':
