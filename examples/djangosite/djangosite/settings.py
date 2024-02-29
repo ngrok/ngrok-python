de5606145d9df26a00d30e76dfe41fc9917a7d74
James Szalay <james@ngrok.com>
2024-02-29 01:13:53 +0000

    Load policies from django settings

--- examples/djangosite/djangosite/settings.py
+++ examples/djangosite/djangosite/settings.py
@@ -128,3 +128,26 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+NGROK_CONFIG = {
+    "policies": {
+        "inbound": [
+        ],
+        "outbound": [
+            {
+                "expressions": [],
+                "name": "",
+                "actions": [
+                {
+                    "type": "add-headers",
+                    "config": {
+                    "headers": {
+                        "added-header": "added-header-value"
+                    }
+                    }
+                }
+                ]
+            }
+        ]
+    }
+}
--- ngrok_extra/ngrok_extra/django/listener.py
+++ ngrok_extra/ngrok_extra/django/listener.py
@@ -1,10 +1,24 @@
+import json
 from django.conf import settings
 
 async def setup(listen="localhost:8000"):
     import ngrok
     # Note (james): This is where we can get settings for this app and pass them in if we want to.
-    # ngrok_config = getattr(settings, "NGROK_CONFIG", {})
     # If hot reload is on though we'll need to handle that some other way.
-    listener = await ngrok.default()
+    ngrok_config = getattr(settings, "NGROK_CONFIG", {})
+    listener = None
+    if ngrok_config:
+        listener = await listener_from_config(ngrok_config)
+    else:
+        listener = await ngrok.default()
     print(f"Forwarding to {listen} from ingress url: {listener.url()}")
     listener.forward(listen)
+
+async def listener_from_config(config):
+    import ngrok
+    session = await ngrok.SessionBuilder().authtoken_from_env().connect()
+    listener = session.http_endpoint()
+    policies = config.get("policies", {})
+    if policies:
+        listener = listener.policy(json.dumps(policies))
+    return await listener.listen()
